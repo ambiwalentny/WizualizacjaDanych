d08da5b5656914b1cbadc175aed70c7f8f3acfe6
Bartuzi <bartuzi97@gmail.com>
2020-02-27 11:01:41 +0100

    abc

--- ćw1.py
+++ ćw1.py
@@ -1,1 +1,16 @@
-start.py
+liczba= 0
+liczba="0"
+liczba= 0.0
+#kazda zmienna jest obiektem
+#object klasa bazowa
+print(liczba + liczba)
+print ("Ala")
+# alt+shift+strzalka gora (lub dol) kopiowanie zaznczonej linjki powyzej (lub ponizej)
+# ctrl+/ zakomentowanie zaznaczonej lini
+#konwencja co do zmienny ' = ' lepsze niz '='
+#konwencja nazw podlogi przy dlugich nazwach 'dluga_nazwa_zmiennej
+
+def funkcja():
+    print(liczba + liczba)
+    print ("Ala")
+    print(liczba)
